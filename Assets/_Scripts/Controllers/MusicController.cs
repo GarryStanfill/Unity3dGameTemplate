628f1d3b3b88fbfa9e40c7f721f5dcf19a85cae2
GarryStanfill <garry.stanfill@gmail.com>
2020-04-13 16:09:08 -0500

    Initial Commit

---
+++ Assets/_Scripts/Controllers/MusicController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Controls music playback
+/// </summary>
+[RequireComponent(typeof(AudioSource))]
+public class MusicController : MonoBehaviour
+{
+    /// <summary>
+    /// All songs in the Resources/Music folder at runtime
+    /// </summary>
+    List<Song> allSongs     = new List<Song>();
+    /// <summary>
+    /// All songs marked as enabled in the Resources/Music folder at runtime
+    /// </summary>
+    List<Song> activeSongs  = new List<Song>();
+    ///#IGNORE
+    void Start()
+    {
+        LoadAudioClips();
+    }
+
+    /// <summary>
+    /// Loads the Song scriptable objects present in the Resources/Music folder at runtime 
+    /// </summary>
+    void LoadAudioClips()
+    {
+        Song[] clips = Resources.LoadAll<Song>("Music");
+        foreach (Song clip in clips)
+        {
+            allSongs.Add(clip);
+            if (clip.enabled)
+                activeSongs.Add(clip);
+        }
+    }
+}
---
+++ Assets/_Scripts/ScriptableObjects/Song.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+/// <summary>
+/// Song scriptable object
+/// </summary>
+[CreateAssetMenu(fileName = "Song", menuName = "New/Song")]
+public class Song : ScriptableObject
+{
+    /// <summary>
+    /// The name of the audio clip
+    /// </summary>
+    public string songName;
+    /// <summary>
+    /// Audioclip reference
+    /// </summary>
+    public AudioClip song;
+    /// <summary>
+    /// Length of the song.
+    /// <remarks>Extrapolated when OnEnable executes</remarks>
+    /// </summary>
+    public double playTime;
+    /// <summary>
+    /// Is this song clip enabled?
+    /// </summary>
+    public bool enabled = true;
+    ///#IGNORE
+    private void OnEnable()
+    {
+        playTime = song.length;
+    }
+}
