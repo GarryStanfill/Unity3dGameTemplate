95e8613a9a275bfb4eade412927ea543961bcd0c
GarryStanfill <garry.stanfill@gmail.com>
2020-04-14 10:43:50 -0500

    Sound Effects slider updates volume in real time

---
+++ Assets/_Scripts/Controllers/AudioController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+/// <summary>
+/// Controls audio playback sans music
+/// </summary>
+[RequireComponent(typeof(AudioSource))]
+public class AudioController : MonoBehaviour
+{
+    ///#IGNORE
+    AudioSource audioSource;
+    /// <summary>
+    /// The AudioMixerGroup that this controller uses to route clip output.  
+    /// </summary>
+    public AudioMixerGroup audioMixerGroup;
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioMixerGroup = audioSource.outputAudioMixerGroup;
+    }
+    /// #IGNORE
+    void OnEnable()
+    {
+
+    }
+}
