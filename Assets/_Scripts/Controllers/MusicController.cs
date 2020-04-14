3389cfb16e6195c54c8bf5e8f70e026c4dd6d7fb
GarryStanfill <garry.stanfill@gmail.com>
2020-04-14 08:58:46 -0500

    MusicController plays audio clip at runtime if available

--- Assets/_Scripts/Controllers/MusicController.cs
+++ Assets/_Scripts/Controllers/MusicController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -9,6 +10,14 @@ using UnityEngine;
 public class MusicController : MonoBehaviour
 {
     /// <summary>
+    /// Song scriptable object of the currently playing audio clip
+    /// </summary>
+    public Song currentSong;
+    /// <summary>
+    /// Amount of time to wait between end of currentSong and start of next song
+    /// </summary>
+    public float delayBetweenSongs = 0;
+    /// <summary>
     /// All songs in the Resources/Music folder at runtime
     /// </summary>
     List<Song> allSongs     = new List<Song>();
@@ -17,11 +26,19 @@ public class MusicController : MonoBehaviour
     /// </summary>
     List<Song> activeSongs  = new List<Song>();
     ///#IGNORE
-    void Start()
+    ///
+    AudioSource audioSource;
+    /// #IGNORE
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+    /// #IGNORE
+    void OnEnable()
     {
         LoadAudioClips();
+        PlayRandomMusicAudioClip();
     }
-
     /// <summary>
     /// Loads the Song scriptable objects present in the Resources/Music folder at runtime 
     /// </summary>
@@ -34,5 +51,15 @@ public class MusicController : MonoBehaviour
             if (clip.enabled)
                 activeSongs.Add(clip);
         }
+    }
+    /// <summary>
+    /// Select a song from the activeSongs list and play it, recursively invokes to start a new song when clip finishes + delayBetweenSongs value
+    /// </summary>
+    private void PlayRandomMusicAudioClip()
+    {
+        currentSong = activeSongs[UnityEngine.Random.Range(0, activeSongs.Count)];
+        audioSource.clip = currentSong.song;
+        audioSource.Play();
+        Invoke("PlayRandomMusicAudioClip", (float)currentSong.playTime + delayBetweenSongs);
     }
 }
